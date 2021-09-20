af3d473e16663623c698e20a38d72ff94bd44810
abdallah-alaff96 <64463982+abdallah-alaff96@users.noreply.github.com>
2021-09-21 01:42:38 +0300

    commit-1 modification

--- script.js
+++ script.js
@@ -4,17 +4,21 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+// 244. our first AJAX call: XMLHttpRequest
+
 // AJAX
 // 1- The most old school of AJAX is : XML HTTP request function
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.eu/rest/v2/name/portugal');
-request.send();
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText); // destructuing, because its an object inside an array
-  console.log(data);
+const getCountryDate = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText); // destructuing, because its an object inside an array
+    console.log(data);
 
-  //   create a templet leteral :
-  const html = `
+    //   create a templet leteral :
+    const html = `
      <article class="country">
           <img class="country__img" src="${data.flag}" />
           <div class="country__data">
@@ -31,6 +35,16 @@ request.addEventListener('load', function () {
      </article>
   `;
 
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-});
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+// Note that the order of the following function calls will not be in that order due to 'load' event, the first one will appear at first then the second and so on...
+// to solve this issue, we have to "chain the requests" (will be taken next lecture)
+getCountryDate('portugal');
+getCountryDate('spain');
+getCountryDate('egypt');
+getCountryDate('turkey');
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
