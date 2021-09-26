410154ec8d93e8afee14f98f98ea578717704355
abdallah-alaff96 <64463982+abdallah-alaff96@users.noreply.github.com>
2021-09-26 21:59:13 +0300

    Handling Rejected Promises === error
    The following is included:
    
    - .catch()
    - .finally()
    - console.error()
    - insertAdjacentText()
    - err.message

--- script.js
+++ script.js
@@ -3,6 +3,34 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+// to handle pormise errors
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1; moved to finally() method
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+          <img class="country__img" src="${data.flags[0]}" />
+          <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              data.population / 1000000
+            ).toFixed(1)} People</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+          </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1; moved to finally() method
+};
+
 ///////////////////////////////////////
 
 // 244. our first AJAX call: XMLHttpRequest
@@ -57,28 +85,6 @@ const countriesContainer = document.querySelector('.countries');
 //246. Welcome to CallBack Hell
 // in this lecutre it runs in (sequence) not in paraller as previous ex
 
-const renderCountry = function (data, className = '') {
-  const html = `
-  <article class="country ${className}">
-          <img class="country__img" src="${data.flags[0]}" />
-          <div class="country__data">
-          <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              data.population / 1000000
-            ).toFixed(1)} People</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${
-              data.currencies[0].name
-            }</p>
-          </div>
-          </article>
-  `;
-
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
-
 // const getCountryAndNeighbour = function (country) {
 //   const request = new XMLHttpRequest();
 //   // request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
@@ -126,20 +132,20 @@ const renderCountry = function (data, className = '') {
 // 249. Chanining Promises - The Best Practice
 
 // for country >> neighbor
-const getCountrydata = function (country) {
-  fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(response => response.json())
-    .then(data => {
-      renderCountry(data[0]);
-      const neighbor = data[0].borders[0];
-      // return the fetch output (promise) and write .then .then outside to avoid calling a callback fucntion inside a callback function (to avoid callback Hell)
-      return fetch(`https://restcountries.com/v2/alpha/${neighbor}`);
-    })
-    .then(response => response.json())
-    .then(data => renderCountry(data, 'neighbor'));
-};
+// const getCountrydata = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(response => response.json())
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbor = data[0].borders[0];
+//       // return the fetch output (promise) and write .then .then outside to avoid calling a callback fucntion inside a callback function (to avoid callback Hell)
+//       return fetch(`https://restcountries.com/v2/alpha/${neighbor}`);
+//     })
+//     .then(response => response.json())
+//     .then(data => renderCountry(data, 'neighbor'));
+// };
 
-getCountrydata('portugal');
+// getCountrydata('portugal');
 
 // for country >> neighbor >> neighbor the neighbor
 // const getCountrydata = function (country) {
@@ -161,3 +167,29 @@ getCountrydata('portugal');
 // };
 
 // getCountrydata('portugal');
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 250. Handling Rejected Promises === error
+
+const getCountrydata = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbor = data[0].borders[0];
+      return fetch(`https://restcountries.com/v2/alpha/${neighbor}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbor'))
+    .catch(err => {
+      console.error(err);
+      renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1; // instead of loading spinner which used with asycn actions
+    });
+};
+
+btn.addEventListener('click', function (e) {
+  getCountrydata('portugal');
+});
