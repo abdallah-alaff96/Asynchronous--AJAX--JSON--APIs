772ce4662d67620f9da68f6ba367df66ba7a4f98
abdallah-alaff96 <64463982+abdallah-alaff96@users.noreply.github.com>
2021-10-29 14:48:48 +0300

    262.Running Promises in Parallel

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const renderCountry = function (data, className = '') {
 
 // const getCountrydata = function (country) {
 //   const request = new XMLHttpRequest();
-//   // request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+//   // request.open('GET', `https://restcountries.com/rest/v2/name/${country}`);
 //   request.open('GET', `https://restcountries.com/v2/name/${country}`);
 //   request.send();
 //   request.addEventListener('load', function () {
@@ -88,7 +88,7 @@ const renderCountry = function (data, className = '') {
 
 // const getCountryAndNeighbour = function (country) {
 //   const request = new XMLHttpRequest();
-//   // request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+//   // request.open('GET', `https://restcountries.com/rest/v2/name/${country}`);
 //   request.open('GET', `https://restcountries.com/v2/name/${country}`);
 //   request.send();
 //   request.addEventListener('load', function () {
@@ -200,13 +200,13 @@ const renderCountry = function (data, className = '') {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 251.Throwing Errors Manually  -  to fix "404" error
 
-// const getJSON = function (url, errorMsg = 'Something went wrong') {
-//   return fetch(url).then(response => {
-//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
 
-//     return response.json();
-//   });
-// };
+    return response.json();
+  });
+};
 
 // const getCountrydata = function (country) {
 //   getJSON(`https://restcountries.com/v2/name/${country}`, 'Country not found')
@@ -473,38 +473,38 @@ const wait = function (seconds) {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 261.Returning values from Async Functions
 
-const getPosition = function () {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-};
+// const getPosition = function () {
+//   return new Promise(function (resolve, reject) {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
 
-const whereAmI = async function () {
-  try {
-    // Geolocation
-    const pos = await getPosition();
-    const { latitude: lat, longitude: lng } = pos.coords;
+// const whereAmI = async function () {
+//   try {
+//     // Geolocation
+//     const pos = await getPosition();
+//     const { latitude: lat, longitude: lng } = pos.coords;
 
-    // Reverse Geocoding
-    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    if (!resGeo.ok) throw new Error('Problem getting location data');
-    const dataGeo = resGeo.json();
+//     // Reverse Geocoding
+//     const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+//     if (!resGeo.ok) throw new Error('Problem getting location data');
+//     const dataGeo = resGeo.json();
 
-    //Country Data
-    const res = await fetch(
-      `https://restcountries.com/v2/name/${dataGeo.country}`
-    );
-    if (!res.ok) throw new Error('Problem getting country');
-    const data = await res.json();
-    renderCountry(data);
-  } catch (err) {
-    console.error(`${err.message} 💥`);
-    // Reject Promise returned from async function
-    // Rethrow Error
-    throw err;
-  }
-};
-console.log('1: will get location');
+//     //Country Data
+//     const res = await fetch(
+//       `https://restcountries.com/v2/name/${dataGeo.country}`
+//     );
+//     if (!res.ok) throw new Error('Problem getting country');
+//     const data = await res.json();
+//     renderCountry(data);
+//   } catch (err) {
+//     console.error(`${err.message} 💥`);
+//     // Reject Promise returned from async function
+//     // Rethrow Error
+//     throw err;
+//   }
+// };
+// console.log('1: will get location');
 
 // solution 1 old way (using then().catch())
 {
@@ -516,14 +516,39 @@ console.log('1: will get location');
   // console.log('3: Finished getting location');
 }
 
-// solution 2 - new way - using (Async/Await)
-// the next is an async X function (the same async function consept)
-(async function () {
+// solution 2 - new way - using (Async/Await) - IIFE
+// the next is an async IIFE function (the same async function consept)
+// (async function () {
+//   try {
+//     const city = await whereAmI();
+//     console.log(`2:${city}`);
+//   } catch (err) {
+//     console.error(`2: ${err.message} 💥💥 after rethrow error`);
+//   }
+//   console.log('3: Finished getting location');
+// })();
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 262.Running Promises in Parallel
+
+const get3Countries = async function (c1, c2, c3) {
   try {
-    const city = await whereAmI();
-    console.log(`2:${city}`);
+    // // solution1 - the following 3 data will load in series - take much time
+    // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    // solution 2
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
   } catch (err) {
-    console.error(`2: ${err.message} 💥💥 after rethrow error`);
+    console.error(err);
   }
-  console.log('3: Finished getting location');
-})();
+};
+
+get3Countries('palestine', 'canada', 'portugal');
